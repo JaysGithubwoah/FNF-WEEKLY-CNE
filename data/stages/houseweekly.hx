898b0856c20a800de210568e6cac7d9a3ab72994
NoelleDeSpell <cxdeakins@gmail.com>
2025-02-13 00:39:55 -0500

    weekly mix improvem.ents i think,.

--- data/stages/houseweekly.hx
+++ data/stages/houseweekly.hx
@@ -1,18 +1,15 @@
-import flixel.effects.FlxFlicker;
-import flixel.text.FlxText;
-import flixel.text.FlxTextBorderStyle;
-import flixel.text.FlxTextAlign;
-
+//sprite vars
 var bfjumpscare:FlxSprite;
 var barTop:FlxSprite;
 var barBottom:FlxSprite;
 var prostate:FlxText;
 var kinoTween:FlxTween;
+//pathing
 var path1:String = 'bg/tweaking/';
 var path2:String = 'bg/tweaking/weeklyMix/';
-function postCreate(){
-//bg size
-bg.setGraphicSize(bg.width * 1.75);
+
+function create(){
+//yo can someone resize the combo thing for me it's too big and i can't figure it out
 
 //bf jumpscare
 bfjumpscare = new FlxSprite(0, 0).loadGraphic(Paths.image(path1 + "jumpscare!"));
@@ -38,72 +35,35 @@ quote2 = new FlxSprite(500, 300).loadGraphic(Paths.image(path2 + "quote2"));
 quote2.cameras = [camHUD];
 quote2.alpha = 0;
 add(quote2);
+
 //ahhh!!
 bossjumpscare = new FlxSprite(0, 0).loadGraphic(Paths.image(path2 + "bossjumpscare"));
 bossjumpscare.cameras = [camHUD];
 bossjumpscare.alpha = 0;
-add(bossjumpscare);
-
-//you knows what crazy? that low taper fade meme is still MASSIVE dude. massive. IT IS!
-barTop = new FlxSprite().makeGraphic(Std.int(FlxG.width * 2), Std.int(68), FlxColor.BLACK);
-barTop.screenCenter(FlxAxes.X);
-barTop.cameras = [camHUD];
-barTop.y = 0 - barTop.height;
-barBottom = new FlxSprite().makeGraphic(Std.int(FlxG.width * 2), Std.int(68), FlxColor.BLACK);
-barBottom.screenCenter(FlxAxes.X);
-barBottom.cameras = [camHUD];
-barBottom.y = 652 + barBottom.height; //652
-insert(getObjectOrder(healthBarBG) - 12, barTop);
-insert(getObjectOrder(healthBarBG) - 12, barBottom);
-add(barTop);
-add(barBottom);
-
-//kino text
-prostate = new FlxText(1280, 100);
-prostate.cameras = [camHUD];
-prostate.setFormat(Paths.font("vcr.ttf"), 48);
-prostate.text = 'kino.hx ACTIVATED';
-prostate.alignment = FlxTextAlign.CENTER;
-prostate.borderStyle = FlxTextBorderStyle.OUTLINE;
-prostate.color = 0xFFFFFFFF;
-prostate.borderColor = 0xFF000000;
-prostate.width = 100;
-prostate.antialiasing = false;
-prostate.updateHitbox();
-prostate.screenCenter();
-prostate.borderSize = 3;
-prostate.visible = false;
-add(prostate);
-}
+add(bossjumpscare);}
 
 //jumpscare function
 public function bfJumpscare(){
 bfjumpscare.alpha = 1;
 new FlxTimer().start(0.1, function(tmr:FlxTimer){
 bfjumpscare.alpha = 0;});}
+
 //funny goo weekly functions
 public function bossJumpscare(){
 bossjumpscare.alpha = 1;}
 public function peace(){
 FlxTween.tween(quoteBG, {alpha: 1}, 7.5, {ease: FlxEase.expoOut});}
+
 //something about these is weird i feel like but im so tired im not working on this no more
 public function quote1(){
-FlxTween.tween(quote1, {alpha: 1}, 7.5, {ease: FlxEase.expoOut});}
+FlxTween.tween(quote1, {alpha: 1}, 2, {ease: FlxEase.linear});}
 public function quote2(){
-FlxTween.tween(quote2, {alpha: 1}, 8.5, {ease: FlxEase.expoOut});}
-//funny bar functions
-public function funnyBars() {
-FlxTween.tween(barTop, {y: 0}, 1, {ease: FlxEase.expoOut});
-FlxTween.tween(barBottom, {y: 652}, 1, {ease: FlxEase.expoOut});
-prostate.visible = true;
-FlxFlicker.flicker(prostate, 2, 0.25, false);}
-public function funnyBarsOff(){
-FlxTween.tween(barTop, {y: 0 - 68}, 1, {ease: FlxEase.expoOut});
-FlxTween.tween(barBottom, {y: 652 + 68}, 1, {ease: FlxEase.expoOut});}
-//thing for bars behind hud
-function getObjectOrder(obj:Dynamic) {
-return PlayState.instance.members.indexOf(obj);}
+FlxTween.tween(quote2, {alpha: 1}, 2, {ease: FlxEase.linear});}
 
-/*
-i wanted to do icon bop stuff here but i just couldn't figure it out
-*/
+//icon bops
+function beatHit(){
+var targetRotate:Int = curBeat / 2;
+if (curBeat % 2 == 0) {
+targetRotate *= -1;}
+FlxTween.angle(iconP1, targetRotate, 0, 0.3, {ease: FlxEase.quadOut});
+FlxTween.angle(iconP2, targetRotate, 0, 0.3, {ease: FlxEase.quadOut});}
--- songs/Goo Weekly Mix/scripts/script.hx
+++ songs/Goo Weekly Mix/scripts/script.hx
@@ -1,22 +1,69 @@
-import funkin.backend.scripting.events.StateEvent;
+/*
+goo specific stuff was added here because i didn't like the stage file being so damn big
+and also the quote tweens are still broken oops!
+it's 12 30 am im tired
+*/
+//imports
+import flixel.effects.FlxFlicker;
+import flixel.text.FlxText;
+import flixel.text.FlxTextBorderStyle;
+import flixel.text.FlxTextAlign;
+function create(){
+camHUD.alpha = 0;}
 //kino fade in intro
 introLength = 0;
 function onCountdown(event){
 event.cancel();
-camHUD.fade(FlxColor.BLACK, 0.01);}
+camera.fade(FlxColor.BLACK, 0.01);}
 function onSongStart(){
-camHUD.fade(FlxColor.BLACK, 7.75, true);}
-
-function beatHit() {
+camera.fade(FlxColor.BLACK, 7.75, true);}
+function beatHit(){
 switch(curBeat){
+case 15: FlxTween.tween(camHUD, {alpha: 1}, 0.5, {ease: FlxEase.linear});
 case 112: funnyBars();
 case 144: funnyBarsOff();
 case 224: camGame.alpha = 0;
-case 226: camHUD.alpha = 1; peace();
+case 226: peace();
 case 227: quote1();
 case 229: quote2();}}
-
 function stepHit(curStep:Int){
 switch(curStep){
-case 942: bossJumpscare();}
-}
+case 942: bossJumpscare();}}
+function postCreate(){
+//kino text
+prostate = new FlxText(1280, 100);
+prostate.cameras = [camHUD];
+prostate.setFormat(Paths.font("vcr.ttf"), 48, FlxColor.WHITE, FlxTextAlign.CENTER, FlxTextBorderStyle.OUTLINE, 0xFF000000);
+prostate.text = 'kino.hx ACTIVATED';
+prostate.width = 100;
+prostate.antialiasing = false;
+prostate.updateHitbox();
+prostate.screenCenter();
+prostate.borderSize = 3;
+prostate.visible = false;
+add(prostate);
+//you knows what crazy? that low taper fade meme is still MASSIVE dude. massive. IT IS!
+barTop = new FlxSprite().makeGraphic(Std.int(FlxG.width * 2), Std.int(68), FlxColor.BLACK);
+barTop.screenCenter(FlxAxes.X);
+barTop.cameras = [camHUD];
+barTop.y = 0 - barTop.height;
+barBottom = new FlxSprite().makeGraphic(Std.int(FlxG.width * 2), Std.int(68), FlxColor.BLACK);
+barBottom.screenCenter(FlxAxes.X);
+barBottom.cameras = [camHUD];
+barBottom.y = 652 + barBottom.height; //652
+insert(getObjectOrder(healthBarBG) - 12, barTop);
+insert(getObjectOrder(healthBarBG) - 12, barBottom);
+add(barTop);
+add(barBottom);}
+//funny bar functions
+function funnyBars() {
+FlxTween.tween(barTop, {y: 0}, 1, {ease: FlxEase.expoOut});
+FlxTween.tween(barBottom, {y: 652}, 1, {ease: FlxEase.expoOut});
+prostate.visible = true;
+FlxFlicker.flicker(prostate, 2, 0.25, false);}
+function funnyBarsOff(){
+FlxTween.tween(barTop, {y: 0 - 68}, 1, {ease: FlxEase.expoOut});
+FlxTween.tween(barBottom, {y: 652 + 68}, 1, {ease: FlxEase.expoOut});}
+//thing for bars behind hud
+function getObjectOrder(obj:Dynamic){
+return PlayState.instance.members.indexOf(obj);}
